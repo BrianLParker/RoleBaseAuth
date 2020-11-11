ea909c5d86b3504b00d376065808df7c75d6f305
Brian Parker <brian.parker@hotmail.com.au>
2020-11-12 03:20:17 +1100

    database migration Planet property

---
+++ RoleBaseAuth/Server/Data/Migrations/20201111161854_AddPlanetClaimProperty.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RoleBaseAuth.Server.Data.Migrations
+{
+    public partial class AddPlanetClaimProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Planet",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Planet",
+                table: "AspNetUsers");
+        }
+    }
+}
--- RoleBaseAuth/Server/Models/ApplicationUser.cs
+++ RoleBaseAuth/Server/Models/ApplicationUser.cs
@@ -1,12 +1,12 @@
 ﻿namespace RoleBaseAuth.Server.Models
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations;
 
     public class ApplicationUser : IdentityUser
     {
+        [MaxLength(256)]
+        [Required(AllowEmptyStrings = false)]
+        public string Planet { get; set; }
     }
 }
