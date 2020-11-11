258e360477185bd8f7d17aabce6fe246b969d613
Brian Parker <brian.parker@hotmail.com.au>
2020-11-12 03:26:44 +1100

    Scaffold identity Register and Manage pages

---
+++ RoleBaseAuth/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RoleBaseAuth.Server.Data;
+using RoleBaseAuth.Server.Models;
+
+[assembly: HostingStartup(typeof(RoleBaseAuth.Server.Areas.Identity.IdentityHostingStartup))]
+namespace RoleBaseAuth.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
